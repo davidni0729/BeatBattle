3bc8c8f56ab4da0cfd9f70a70f77619db4a55e77
DavidNi <davidni0729@gmail.com>
2018-09-05 14:26:09 +0800

    Fix: controller mouse press twice issue, and  change controller style

--- public/controller.js
+++ public/controller.js
@@ -6,13 +6,19 @@
 var socket;
 var btnWidth;
 var btn1Color;
+var btn1OverlayAlpha;
 var btn1x;
 var btn2Color;
+var btn2OverlayAlpha;
 var btn2x;
 var btn3Color;
+var btn3OverlayAlpha;
 var btn3x;
 var btn4Color;
+var btn4OverlayAlpha;
 var btn4x;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,14 +26,20 @@ function setup() {
 
 
   btnWidth = windowWidth/4;
-  btn1Color = 0;
+  btn1Color = color(190,194,63);
+  btn1OverlayAlpha = 0;
   btn1x = 0; 
-  btn2Color = 0;
+  btn2Color = color(232,122,144);
+  btn2OverlayAlpha = 0;
   btn2x = btnWidth;
-  btn3Color = 0;
+  btn3Color = color(88,178,220);
+  btn3OverlayAlpha = 0;
   btn3x = btnWidth * 2;
-  btn4Color = 0;
+  btn4Color = color(152,109,178);
+  btn4OverlayAlpha = 0;
   btn4x = btnWidth * 3;
+
+
 
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
@@ -38,62 +50,73 @@ function setup() {
 function draw() {
   background(0);
 
-  if(btn1Color > 0) btn1Color -= 51;
-  if(btn2Color > 0) btn2Color -= 51;
-  if(btn3Color > 0) btn3Color -= 51;
-  if(btn4Color > 0) btn4Color -= 51;
+  if(btn1OverlayAlpha > 0) btn1OverlayAlpha -= 20;
+  if(btn2OverlayAlpha > 0) btn2OverlayAlpha -= 20;
+  if(btn3OverlayAlpha > 0) btn3OverlayAlpha -= 20;
+  if(btn4OverlayAlpha > 0) btn4OverlayAlpha -= 20;
 
-  
-  fill(btn1Color, btn1Color, btn1Color);
+
+  fill(btn1Color);
+  rect(btn1x, 0, btnWidth, windowHeight);
+  fill(255,255,255,btn1OverlayAlpha);
   rect(btn1x, 0, btnWidth, windowHeight);
 
-  fill(btn2Color, btn2Color, btn2Color);
-  rect(btn2x, 0, btnWidth, windowHeight);
 
-  fill(btn3Color, btn3Color, btn3Color);
+  fill(btn2Color);
+  rect(btn2x, 0, btnWidth, windowHeight);
+  fill(255,255,255,btn2OverlayAlpha);
+  rect(btn2x, 0, btnWidth, windowHeight);
+  
+  fill(btn3Color);
+  rect(btn3x, 0, btnWidth, windowHeight);
+  fill(255,255,255,btn3OverlayAlpha);
   rect(btn3x, 0, btnWidth, windowHeight);
 
-  fill(btn4Color, btn4Color, btn4Color);
+  fill(btn4Color);
+  rect(btn4x, 0, btnWidth, windowHeight);
+  fill(255,255,255,btn4OverlayAlpha);
   rect(btn4x, 0, btnWidth, windowHeight);
 
 }
 
 
-function touchStarted() {
-  var data = {
-    btn1: false,
-    btn2: false,
-    btn3: false,
-    btn4: false
-  };
+function mousePressed() {
+  if(event.type == 'mousedown'){
 
-  if(mouseX>=0 && mouseX<btnWidth){
-    btn1Color = 255;
-    data.btn1 = true;
-    console.log("data:", data);
-    socket.emit('button',data);
-  }
-  if(mouseX>=btnWidth && mouseX<btnWidth*2){
-    btn2Color = 255;
-    data.btn2 = true;
-    console.log("data:", data);
-    socket.emit('button',data);
+    var data = {
+        btn1: false,
+        btn2: false,
+        btn3: false,
+        btn4: false
+      };
 
-  }
+      if(mouseX>=0 && mouseX<btnWidth){
+        btn1OverlayAlpha = 255;
+        data.btn1 = true;
+        console.log("data:", data);
+        socket.emit('button',data);
+      }
 
-  if(mouseX>=btnWidth*2 && mouseX<btnWidth*3){
-    btn3Color = 255;
-    data.btn3 = true;
-    console.log("data:", data);
-    socket.emit('button',data);
+      if(mouseX>=btnWidth && mouseX<btnWidth*2){
+        btn2OverlayAlpha = 255;
+        data.btn2 = true;
+        console.log("data:", data);
+        socket.emit('button',data);
+      }
 
-  }
-  
-  if(mouseX>=btnWidth*3 && mouseX<btnWidth*4){
-    btn4Color = 255;
-    data.btn4 = true;
-    console.log("data:", data);
-    socket.emit('button',data);
+      if(mouseX>=btnWidth*2 && mouseX<btnWidth*3){
+        btn3OverlayAlpha = 255;
+        data.btn3 = true;
+        console.log("data:", data);
+        socket.emit('button',data);
+      }
+      
+      if(mouseX>=btnWidth*3 && mouseX<btnWidth*4){
+        btn4OverlayAlpha = 255;
+        data.btn4 = true;
+        console.log("data:", data);
+        socket.emit('button',data);
+      }
 
   }
 
--- public/sketch.js
+++ public/sketch.js
@@ -30,56 +30,7 @@ createCanvas(1200, 400);
   ghost4 = createSprite(200+shift*3, 200);
   ghost4.addAnimation('normal', 'assets/ghost_spin0001.png', 'assets/ghost_spin0003.png');
   ghost4.mouseActive = true;
-  // asterisk = createSprite(600, 200);
-  // asterisk.addAnimation('normal', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
-  // asterisk.addAnimation('stretch', 'assets/asterisk_stretching0001.png', 'assets/asterisk_stretching0008.png');
-  // var anim = asterisk.addAnimation('transform', 'assets/asterisk_circle0001.png', 'assets/asterisk_circle0008.png');
 
-  // //if defined, the collider will be used for mouse events
-  // asterisk.setCollider('circle', 0, 0, 64);
-
-  // //I can assign functions to be called upon specific mouse events
-  // //within the function "this" will reference the sprite that triggered the event
-  // asterisk.onMouseOver = function() {
-  //   this.changeAnimation('stretch');
-  // };
-
-  // asterisk.onMouseOut = function() {
-  //   this.changeAnimation('normal');
-  // };
-
-  // asterisk.onMousePressed = function() {
-  //   this.changeAnimation('transform');
-  //   this.animation.goToFrame(this.animation.getLastFrame());
-  //   if (draggedSprite == null) {
-  //     draggedSprite = this;
-  //   }
-  // };
-
-  // asterisk.onMouseReleased = function() {
-  //   this.changeAnimation('transform');
-  //   this.animation.goToFrame(0);
-  //   if (draggedSprite == this) {
-  //     draggedSprite = null;
-  //   }
-  // };
-
-  // createCanvas(windowWidth, windowHeight);
-  // //fullScreen();
-  // background(0);
-
-
-  // c1 = 255;
-  // c2 = 255;
-  // w = windowWidth;
-  // h = windowHeight;
-
-
-  // btn1 = new Button(w/2-200,h/2,"red");
-  // btn2 = new Button(w/2+200,h/2,"green");
-
-  //frameRate(50);
-  //println(frameRate);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
    socket = io.connect('http://192.168.43.229:3000');
@@ -92,19 +43,15 @@ createCanvas(1200, 400);
       
         if(data.btn1)
           ghost1.rotation-= 10;
-        ghost1.visible = !ghost1.mouseIsPressed;
 
         if(data.btn2)
           ghost2.rotation-= 10;
-        ghost2.visible = !ghost2.mouseIsPressed;
 
         if(data.btn3)
           ghost3.rotation-= 10;
-        ghost3.visible = !ghost3.mouseIsPressed;
 
         if(data.btn4)
           ghost4.rotation-= 10;
-        ghost4.visible = !ghost4.mouseIsPressed;
 
         drawSprites();
     }
